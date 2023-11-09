74b925605e6dde524ff54143e9008bbdcf3ca553
danjerezTdf <danieljerez.tdf@gmail.com>
2023-11-09 20:44:57 -0300

    enable_cors

--- my-list-back/src/main.ts
+++ my-list-back/src/main.ts
@@ -1,12 +1,19 @@
+import { Module } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+@Module({
+  imports: [AppModule],
+})
+export class MainModule {}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(MainModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'https://mylist-a570c.web.app', // Reemplaza con tu dominio
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
-  await app.listen(3002);
+  await app.listen(3001);
 }
 bootstrap();
